e3625f9d5e9287bd3cc29c82c513ea9b032a4f0e
pathum-liyanagama <pathumpml.dev@gmail.com>
2020-12-10 16:25:08 +0530

    Add cart dropdown empty message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,8 +8,13 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = ({ cartItems }) => {
     return (
         <div className="cart-dropdown">
+
             <div className="cart-items">
-                {cartItems.map((item) => <CartItem key={item.id} item={item} />)}
+                {
+                    cartItems.length ?
+                        cartItems.map((item) => <CartItem key={item.id} item={item} />)
+                        : <span className="empty-message">Your cart is empty</span>
+                }
             </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
